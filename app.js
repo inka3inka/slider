1600f5f0e46a652d67d8354f75704a098c35781b
inka3inka <inka3inka@gmail.com>
2020-05-28 02:27:45 +0200

    Prepared all app structure: (1) show random photo on the landing screen, (2) load gallery from selected user, (3) Button handlers: remove welcome screen, remove previous search results, input validation

--- app.js
+++ app.js
@@ -1,35 +1,76 @@
 import './main.scss';
 
-let button = document.querySelector(".btn");
+let clientId = "OAX7jYHyzy2x5AYlCaB7Ia5X_2q-NTLHTNmmXwbws2E";
+const button = document.querySelector(".btn");
+const carouselInner = document.querySelector(".carousel-inner");
+const gallery = document.querySelector(".gallery");
+const welcomeTile = document.querySelector('.welcome-tile');
+let userName = document.querySelector("#search");
 
+//Set random photo for a first user's view
+function setRandomPhoto() {
+  let newWelcomePhoto = document.createElement("img")
+  fetch(`https://api.unsplash.com/photos/random/?client_id=${clientId}`)
+    .then(resp => resp.json())
+    .then(photo => {
+        welcomeTile.appendChild(newWelcomePhoto).setAttribute("style", `background-image: url(${photo.urls.regular})`);
+      }
+    )
+}
+
+setRandomPhoto();
+
+
+//Search user's photos
 export function searchPhotos() {
-  let clientId = "OAX7jYHyzy2x5AYlCaB7Ia5X_2q-NTLHTNmmXwbws2E";
-  let query = document.querySelector("#search").value;
-  let url = `https://api.unsplash.com/search/photos/?client_id=${clientId}&query=${query}`;
-
-
+  welcomeTile.innerHTML = "";
+  carouselInner.innerHTML = "";
+  let url = `https://api.unsplash.com/users/${userName.value}/photos/?client_id=${clientId}`;
 
 //request to the API
 
 
-
-fetch(url)
-  .then(resp => resp.json())
-  .then(resp => {
-    console.log(resp);
-    resp.results.map(photo => {
-
-      let gallery = `
-        <img src="${photo.urls.regular}">
-        <a href="${photo.links.download}"></a>
-      `
-
-      document.querySelector(".result").innerHTML = gallery
-
-    })
-  })
-
-
+  fetch(url)
+    .then(resp => resp.json())
+    .then(resp => {
+      console.log(resp);
+      resp.map((photo, idx) => {
+          if (idx == 0) {
+            let galleryElement = `<img style="background-image: url(${photo.urls.regular})" class="d-block w-100">`
+            const newElement = document.createElement("div");
+            carouselInner.appendChild(newElement).innerHTML = galleryElement;
+            newElement.classList.add("carousel-item", "active");
+          } else {
+            let galleryElement = `<img style="background-image: url(${photo.urls.regular})" class="d-block w-100">`
+            const newElement = document.createElement("div");
+            carouselInner.appendChild(newElement).innerHTML = galleryElement;
+            newElement.classList.add("carousel-item");
+          }
+        }
+      )
+    }
+  )
+  userName.value = "";
+  button.setAttribute("disabled", "true");
 }
 
+//Key press effects
+userName.addEventListener('keyup', function(event) {
+  //Search by press Enter key
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+  //Validate input value
+  else if (userName.value.trim() != "") {
+    button.removeAttribute("disabled")
+  }
+  else {
+    button.setAttribute("disabled", "true")
+  }
+})
+
 button.addEventListener("click", searchPhotos);
+button.addEventListener("click", () => gallery.classList.remove("hidden"));
+
+
